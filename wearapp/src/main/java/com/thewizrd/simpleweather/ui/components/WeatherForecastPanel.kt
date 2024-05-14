4c48576e04a22b9414197803dfb0c7d3ef9c975d
Dave Antoine <thewizrd.dev@gmail.com>
2024-05-13 23:04:58 -0400

    wear: some minor ui adjustments

--- wearapp/src/main/java/com/thewizrd/simpleweather/ui/components/WeatherForecastPanel.kt
+++ wearapp/src/main/java/com/thewizrd/simpleweather/ui/components/WeatherForecastPanel.kt
@@ -1,6 +1,13 @@
 package com.thewizrd.simpleweather.ui.components
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
